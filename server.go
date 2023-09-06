672a53bf8c520826734ffb63915eea2dac781778
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2023-09-06 17:33:59 +0600

    first commit

---
+++ server.go
@@ -0,0 +1,25 @@
+package todo_app
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(post string) error {
+	s.httpServer = &http.Server{
+		Addr:           ":" + post,
+		MaxHeaderBytes: 1 << 20, // 1 MB
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
