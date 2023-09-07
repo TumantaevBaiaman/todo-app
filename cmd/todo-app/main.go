eea5fd3a91b85a0f1fca82546da161deaae18843
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2023-09-07 19:38:35 +0600

    add routers

--- cmd/todo-app/main.go
+++ cmd/todo-app/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"log"
+	todo_app "todo-app"
+)
 
+func main() {
+	srv := new(todo_app.Server)
+	if err := srv.Run("8080"); err != nil {
+		log.Fatalf("error occured while running server: %s", err.Error())
+	}
 }
