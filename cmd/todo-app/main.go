3fb790809d3748d65084c463a3271aa457f04603
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2023-09-08 08:22:09 +0600

    add handlers

--- cmd/todo-app/main.go
+++ cmd/todo-app/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	todo_app "todo-app"
+	"todo-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo_app.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running server: %s", err.Error())
 	}
 }
